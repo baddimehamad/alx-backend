1151d632f9378a86e5dde0f2ca84ecb69fda6201
BADDI MOHAMED <mehamad199523@gmail.com>
2024-05-04 10:46:08 -0700

    0x00. Pagination

--- 0x00-pagination/2-hypermedia_pagination.py
+++ 0x00-pagination/2-hypermedia_pagination.py
@@ -16,7 +16,7 @@ def index_range(page: int, page_size: int) -> Tuple[int, int]:
 class Server:
     """Server class to paginate a database of popular baby names.
     """
-    DATA_FILE = "Popular_Baby_Names.csv"
+    DATA_FILE = "names.csv"
 
     def __init__(self):
         self.__dataset = None
